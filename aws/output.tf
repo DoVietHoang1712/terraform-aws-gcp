b84895c2bb764f316139dfdd4019de332ce50e4f
DoVietHoang1712 <hoang171220001@gmail.com>
2022-12-25 22:56:27 +0700

    feat: Update path

--- aws/output.tf
+++
@@ -1,9 +0,0 @@
-output "VPC_ID" {
-  value = module.vpc.vpc.id
-}
-output "Pirvate_subnet" {
-  value = module.vpc.public_subnet
-}
-output "Route_table" {
-  value = module.aws-vpm-gcp.route_table
-}
