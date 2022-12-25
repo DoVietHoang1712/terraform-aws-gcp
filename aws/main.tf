812ee2277220f68d8d943854e63d77bffb7b0009
DoVietHoang1712 <hoang171220001@gmail.com>
2022-12-25 20:39:17 +0700

    feat: Add service account gcp

--- aws/main.tf
+++ aws/main.tf
@@ -2,24 +2,31 @@ locals {
   project_name = "terraform-aws"
 }
 
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "Admin-role"
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
+}
+
 module "vpc" {
-  source = "./modules/networking"
-  project = local.project_name
-  vpc_cidr = "192.168.0.0/16"
-  subnets = {
-    "private" = ["192.168.1.0/24", "192.168.2.0/24"]
-    "public" = ["192.168.3.0/24", "192.168.4.0/24"]
-    "zone" = ["us-east-1a", "us-east-1b"]
-  }
-  vpc_name = "MyVPC"
+  source   = "./modules/networking"
+  project  = local.project_name
+  vpc_cidr = var.vpc_cidr
+  subnets  = var.aws_subnets
+  vpc_name = var.vpc_name
 }
 
 module "aws-vpm-gcp" {
-  source = "./modules/vpn"
-  vpc_id = module.vpc.vpc.id
+  source         = "./modules/vpn"
+  vpc_id         = module.vpc.vpc.id
   aws_cidr_block = module.vpc.vpc.cidr_block
-  region = "us-east1"
-  account_id = "hoangdv"
-  subnet_id = module.vpc.public_subnet
-  ig_id = module.vpc.igw
+  region         = var.gcp_region
+  account_id     = var.account_id
+  subnet_id      = module.vpc.public_subnet
+  ig_id          = module.vpc.igw
 }
---
+++ aws/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "aws_subnets" {
+  type = any
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "gcp_region" {
+  type = string
+}
