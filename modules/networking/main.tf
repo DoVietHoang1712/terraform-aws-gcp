766be3af73e7ac6990d2d48df915eab758e5f385
HoangDo <hoang.dv@tripi.vn>
2023-01-07 19:07:31 +0700

    Deploy infra

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -102,7 +102,7 @@ resource "aws_security_group" "bastion-sg" {
 
 resource "aws_instance" "bastion" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = var.instance_type
+  instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.bastion-sg.id]
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public_subnet[0].id
