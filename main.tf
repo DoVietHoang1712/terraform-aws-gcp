ccac4cf4ac21d37c0cd8e459692824583aff8e60
DoVietHoang1712 <52753161+DoVietHoang1712@users.noreply.github.com>
2023-01-10 20:21:38 +0700

    Destroy

--- main.tf
+++ main.tf
@@ -13,22 +13,22 @@ provider "aws" {
   secret_key = data.vault_aws_access_credentials.creds.secret_key
 }
 
-module "vpc" {
-  source   = "./modules/networking"
-  project  = local.project_name
-  vpc_cidr = var.vpc_cidr
-  subnets  = var.aws_subnets
-  vpc_name = var.vpc_name
-}
+# module "vpc" {
+#   source   = "./modules/networking"
+#   project  = local.project_name
+#   vpc_cidr = var.vpc_cidr
+#   subnets  = var.aws_subnets
+#   vpc_name = var.vpc_name
+# }
 
-module "aws-vpm-gcp" {
-  source            = "./modules/vpn"
-  vpc_id            = module.vpc.vpc.id
-  aws_cidr_block    = module.vpc.vpc.cidr_block
-  region            = var.gcp_region
-  account_id        = var.account_id
-  private_subnet_id = module.vpc.private_subnet
-  public_subnet_id  = module.vpc.public_subnet
-  ig_id             = module.vpc.igw
-  ng_id             = module.vpc.ngw
-}
+# module "aws-vpm-gcp" {
+#   source            = "./modules/vpn"
+#   vpc_id            = module.vpc.vpc.id
+#   aws_cidr_block    = module.vpc.vpc.cidr_block
+#   region            = var.gcp_region
+#   account_id        = var.account_id
+#   private_subnet_id = module.vpc.private_subnet
+#   public_subnet_id  = module.vpc.public_subnet
+#   ig_id             = module.vpc.igw
+#   ng_id             = module.vpc.ngw
+# }
